14b2f82698c5037c41f61b8c8d358958e4e44561
t-tamaru <t-tamaru@sakura.ad.jp>
2020-08-03 23:10:59 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+// gulpプラグインの読み込み
+const gulp = require("gulp");
+// Sassをコンパイルするプラグインの読み込み
+const sass = require("gulp-sass");
+const packageImporter = require('node-sass-package-importer');
+
+// style.scssの監視タスクを作成する
+gulp.task("default", function () {
+    // ★ style.scssファイルを監視
+    return gulp.watch(
+        "./assets/scss/*.scss",
+        function () {
+            // style.scssの更新があった場合の処理
+
+            // style.scssファイルを取得
+            return (
+                gulp
+                    .src("./assets/scss/*.scss")
+                    // Sassのコンパイルを実行
+                    .pipe(
+                        sass({
+                            outputStyle: "expanded"
+                        })
+                            // Sassのコンパイルエラーを表示
+                            // (これがないと自動的に止まってしまう)
+                            .on("error", sass.logError)
+                    )
+                    // cssフォルダー以下に保存
+                    .pipe(gulp.dest("./"))
+            );
+        }
+    );
+});
