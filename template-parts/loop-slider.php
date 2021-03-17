d2d1ae34266a7f212586d0316ba602164617037f
t-tamaru <t-tamaru@sakura.ad.jp>
2021-03-17 15:10:32 +0900

    ループ情報変更

--- template-parts/loop-slider.php
+++ template-parts/loop-slider.php
@@ -1,13 +1,24 @@
 <div class="swiper-slide">
-  <a href="<?php the_permalink(); ?>">
+  <?php $detail_content = get_sub_field('detail'); ?>
+  <?php if(have_rows('detail')): ?>
+    <a href="<?php the_permalink(); ?>">
+      <?php if (has_post_thumbnail()) { the_post_thumbnail('medium'); } ?>
+    </a>
+  <?php else: ?>
     <?php if (has_post_thumbnail()) { the_post_thumbnail('medium'); } ?>
-  </a>
+  <?php endif; ?>
   <div class="price__productMeta">
     <div>
       <h3 class="price__postTitle -product">
-        <a href="<?php the_permalink(); ?>">
-          <?php the_title(); ?>
-        </a>
+        <?php if(have_rows('detail')): ?>
+          <a href="<?php the_permalink(); ?>">
+            <?php the_title(); ?>
+          </a>
+        <?php
+          else:
+            the_title();
+          endif;
+        ?>
       </h3>
       <?php if (get_field('price')): ?>
         <span class="price__productPrice -pc">
@@ -19,7 +30,7 @@
       <span class="price__productPrice -sp">
         <?php the_field('price'); ?>
       </span>
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/icon_arrow-right.png" alt="" class="price__worksMetaArrow">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/icon_arrow-right.png" alt="" class="price__worksMetaArrow <?php if(!have_rows('detail')){ echo 'nolink'; } ?>">
     </div>
   </div>
 </div>
